0323e6f8cad263e996bff34bbecee975ec22a3d8
hussamkhatib <hussamkhatib20@gmail.com>
2024-03-06 03:31:37 +0400

    wrap

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home({ email }: { email: string }) {
   return (
     <div>
       <h1>EMAIL:, {email}!</h1>
+      <h1>PUBLIC EMAIL: {process.env.NEXT_PUBLIC_EMAIL}</h1>
     </div>
   );
 }
