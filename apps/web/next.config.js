50cc9ce2275f0e80e9dd3393fd1cb3ce04674302
hussamkhatib <hussamkhatib20@gmail.com>
2024-03-06 04:47:36 +0400

    .

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -12,7 +12,7 @@ if (process.env.VERCEL_URL && !process.env.NEXT_PUBLIC_WEBAPP_URL) {
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
+  env:{ 
     NEXT_PUBLIC_WEBAPP_URL: 'https://' + process.env.VERCEL_URL,
   }
 }
